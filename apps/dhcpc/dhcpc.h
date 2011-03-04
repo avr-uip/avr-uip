b516e6fc8b52030dd534d89d61b9baa8068928c1
qarce76 <qarce76@30fab446-694b-0410-acbb-ad9843a15c28>
2011-03-04 06:33:56 +0000

    mod dhcpc to work with port mapper

--- apps/dhcpc/dhcpc.h
+++ apps/dhcpc/dhcpc.h
@@ -62,8 +62,14 @@ void dhcpc_appcall(void);
 
 void dhcpc_configured(const struct dhcpc_state *s);
 
-typedef struct dhcpc_state uip_udp_appstate_t;
-#define UIP_UDP_APPCALL dhcpc_appcall
+
+#if defined PORT_APP_MAPPER
+	#define DHCPC_APP_CALL_MAP {dhcpc_appcall, 68, 0},
+#else
+	#define DHCPC_APP_CALL_MAP
+	#define UIP_UDP_APPCALL dhcpc_appcall
+	typedef struct dhcpc_state uip_udp_appstate_t;
+#endif
 
 
 #endif /* __DHCPC_H__ */
