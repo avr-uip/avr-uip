da73fa16b2286e13ac85c5fa6e6f659a9a72ee5b
qarce76 <qarce76@30fab446-694b-0410-acbb-ad9843a15c28>
2011-12-16 07:31:35 +0000

    check in hacked up version just so there is a copy of it before the cleanup

--- apps/port_app_mapper/port_app_mapper.c
+++ apps/port_app_mapper/port_app_mapper.c
@@ -10,7 +10,7 @@
 #define WEBSERVER_APP_CALL_MAP
 #endif
 
-#ifndef WEBSERVER_APP_CALL_MAP
+#ifndef WEBCLIENT_APP_CALL_MAP
 #define WEBCLIENT_APP_CALL_MAP
 #endif
 
@@ -26,12 +26,16 @@
 #define DHCPC_APP_CALL_MAP
 #endif
 
+#ifndef RESOLV_APP_CALL_MAP
+#define RESOLV_APP_CALL_MAP
+#endif
+
 u8_t myflags;
 struct pt port_thread;
 
 struct port_appcall_map tcp_port_app_map[] = {
 		WEBSERVER_APP_CALL_MAP
-//		WEBCLIENT_APP_CALL_MAP
+		WEBCLIENT_APP_CALL_MAP
 		TELNET_APP_CALL_MAP
 		SIMPLE_HTTPD_APP_CALL_MAP
 		{NULL, 0, 0},
@@ -39,18 +43,38 @@ struct port_appcall_map tcp_port_app_map[] = {
 
 struct port_appcall_map udp_port_app_map[] = {
 		DHCPC_APP_CALL_MAP
-//		RESOLV_APP_CALL_MAP
+		RESOLV_APP_CALL_MAP
 		{NULL, 0, 0},
 	};
+
+// define a basic common type for udp and tcp connections so I can look at the ports
+struct uip_base_conn {
+  uip_ipaddr_t ripaddr;   /**< The IP address of the remote host. */
+
+  u16_t lport;        /**< The local TCP port, in network byte order. */
+  u16_t rport; 
+};
+
+struct uip_base_conn *base_conn;
 
 
 // this function maps the current packet against the mappings
 //   pointed to by the param
 void uip_port_app_mapper(struct port_appcall_map* cur_map)
 {
+char str[60];
 	// save a copy of the current flags as they will change which each appcall
 	myflags = uip_flags;
-
+sendString("\r\n----------------------\r\n");
+sendString("\r\nUIP  CONN INFO \r\n");
+sprintf(str, "UIP - R:%d - L:%d\r\n", (unsigned short)HTONS(uip_conn->rport), (unsigned short)HTONS(uip_conn->lport));
+sendString(str);
+if (base_conn != NULL)
+{
+sprintf(str, "UIP base - R:%d - L:%d\r\n", HTONS(base_conn->rport), HTONS(base_conn->lport));
+sendString(str);
+}
+/*
 	// hack for now.. only dhcp or one udp app will work.
 	if ((cur_map == udp_port_app_map) && 
 		(cur_map->an_appcall != NULL))
@@ -60,13 +84,14 @@ void uip_port_app_mapper(struct port_appcall_map* cur_map)
 
 		return;
 	}
-
+*/
 	// yes this will walk the netire list which is 2 items at the moment
 	while (cur_map->an_appcall != NULL)
 	{
-		// if timeout case, we need to call all apps
-		if (myflags & UIP_TIMEDOUT)
+/*		// if timeout case, we need to call all apps
+		if ((myflags == UIP_TIMEDOUT) || (myflags == UIP_POLL) )
 		{
+sendString("app call timeout case\r\n");
 			// always restore the flags we last had
 			// each time we make the timeout call
 			uip_flags = myflags;
@@ -74,21 +99,29 @@ void uip_port_app_mapper(struct port_appcall_map* cur_map)
 		}
 		else
 		{
+*/sendString("\r\napp call else case\r\n");
+sprintf(str, "R:%d - L:%d\r\n", cur_map->rport, cur_map->lport);
+sendString(str);
 			// Now match the app to the packet.
 			// local AND/OR remote ports match
 			// firs check remote port matches and local may or may not
 			// then check local port matches
 			// can't do it in one statement due to the l & r ports could both be zero and match all apps.
 
-			if (((uip_conn->rport == HTONS(cur_map->rport)) &&
-				 ((cur_map->lport == 0) || (uip_conn->lport == HTONS(cur_map->lport)))) ||
-				((uip_conn->lport == HTONS(cur_map->lport)) &&
-				 ((cur_map->rport == 0) || (uip_conn->rport == HTONS(cur_map->rport)))))
+			if (((base_conn->rport == HTONS(cur_map->rport)) &&
+				 ((cur_map->lport == 0) || (base_conn->lport == HTONS(cur_map->lport)))) ||
+				((base_conn->lport == HTONS(cur_map->lport)) &&
+				 ((cur_map->rport == 0) || (base_conn->rport == HTONS(cur_map->rport)))))
 			{
+sendString("\r\nCON MATCH \r\n");
+sprintf(str, "UIP MATCH - R:%d - L:%d\r\n", HTONS(base_conn->rport), HTONS(base_conn->lport) );
+sendString(str);
+sendString("UIP  CONN INFO \r\n");
 				cur_map->an_appcall();
+base_conn = NULL;
 				break;
 			}
-		}
+//		}
 		cur_map++;
 	}
 }
@@ -97,11 +130,14 @@ void uip_port_app_mapper(struct port_appcall_map* cur_map)
 // pass it the list of apps to match against
 void uip_udp_appcall_port_app_mapper()
 {
+sendString("\r\nudp app call\r\n");
+    base_conn = (struct uip_base_conn *) uip_udp_conn;
 	uip_port_app_mapper(udp_port_app_map);
 }
 
 void uip_appcall_port_app_mapper()
 {
+    base_conn = (struct uip_base_conn *) uip_conn;
 	uip_port_app_mapper(tcp_port_app_map);
 }
 
