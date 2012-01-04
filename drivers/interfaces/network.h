284dd6ab0d32e23ead1ad40b56b2ffaee411a530
qarce76 <qarce76@30fab446-694b-0410-acbb-ad9843a15c28>
2012-01-04 05:32:56 +0000

    add link state check to common api

--- drivers/interfaces/network.h
+++ drivers/interfaces/network.h
@@ -25,4 +25,7 @@ void network_set_MAC(uint8_t* mac);
 /*Gets the MAC address of the device*/
 void network_get_MAC(uint8_t* mac);
 
+/* get the state of the network link on the interface 1 up, 0 down */
+uint8_t network_link_state(void);
+
 #endif /* __NETWORK_H__ */
